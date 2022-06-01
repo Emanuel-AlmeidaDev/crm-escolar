ee236cea55e93d250ba535605a11b5d9a55e887d
Emanuel Almeida <emanuel_almeidadev@outlook.com>
2022-06-01 18:38:05 -0300

    config deploy

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -130,6 +130,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
