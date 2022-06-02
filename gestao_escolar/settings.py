a8c7a7d400cb63c4dabdfd64c80f5ac884836e24
Emanuel Almeida <emanuel_almeidadev@outlook.com>
2022-06-02 09:30:37 -0300

    config settings deploy

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -82,13 +82,19 @@ WSGI_APPLICATION = 'gestao_escolar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+try:
+    from .local_settings import *
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
