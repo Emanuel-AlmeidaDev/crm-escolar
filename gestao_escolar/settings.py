18eb0e6a27154d31c4f993d19c361c69a2a4053a
Emanuel Almeida <emanuel_almeidadev@outlook.com>
2022-06-02 09:57:10 -0300

    config settings deploy

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -86,14 +86,7 @@ try:
     from .local_settings import *
 except ImportError:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT'),
-        }
+        'default': config('HEROKU_POSTGRESQL_YELLOW_URL')
     }
 
 # Password validation
